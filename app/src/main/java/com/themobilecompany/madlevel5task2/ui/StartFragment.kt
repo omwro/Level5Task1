4c2ed337f37cabee3324c5e21bfac3fe2465e885
Omer Erdem <m.o.erdem@outlook.com>
2020-10-10 17:11:31 +0200

    #4.2 connect ui w/ viewmodel

--- app/src/main/java/com/themobilecompany/madlevel5task2/ui/StartFragment.kt
+++ app/src/main/java/com/themobilecompany/madlevel5task2/ui/StartFragment.kt
@@ -6,17 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.themobilecompany.madlevel5task2.R
+import com.themobilecompany.madlevel5task2.model.NoteViewmodel
+import kotlinx.android.synthetic.main.fragment_start.*
+import kotlinx.android.synthetic.main.item_note.*
+import kotlinx.android.synthetic.main.item_note.tvTitle
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class StartFragment : Fragment() {
+    private val viewModel: NoteViewmodel by viewModels()
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false)
@@ -25,8 +32,16 @@ class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        observeAddNoteResult()
+    }
+
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, Observer { note ->
+            note?.let {
+                tvTitle.text = it.title
+                tvLastUpdated.text = getString(R.string.last_updated, it.lastUpdated.toString())
+                tvNote.text = it.text
+            }
+        })
     }
 }
